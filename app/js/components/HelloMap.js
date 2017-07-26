24331b18ae2f294a680e7a32e99a02ba3f1052af
Jon Nordling <jonnordling@gmail.com>
2017-07-26 15:48:07 -0400

    Replacing basemap with vector tiles

--- app/js/components/HelloMap.js
+++ app/js/components/HelloMap.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import Map from 'esri/Map';
 import MapView from 'esri/views/MapView';
+import VectorTileLayer from "esri/layers/VectorTileLayer";
 
 export default class HelloWorld extends Component {
   constructor(){
     super()
   }
   componentDidMount(){
-      var map = new Map({
-      basemap: "topo"
-    });
+      var map = new Map();
 
-    var view = new MapView({
-      container: "map",
-      map: map,
-      center: [-100.33, 25.69],
-      zoom: 10,
-      ui: {
-      components: ["attribution"] // empty the UI, except for attribution
-      }
-    });
+      var view = new MapView({
+        container: "map",
+        map: map,
+        center: [-35.55, 26.53],
+        zoom: 2,
+        ui: {
+          components: ["attribution"] // empty the UI, except for attribution
+        }
+      });
+      var tileLyr = new VectorTileLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/4e1133c28ac04cca97693cf336cd49ad/resources/styles/root.json?f=json"
+      });
+      map.add(tileLyr);
 
   }
   render() {
