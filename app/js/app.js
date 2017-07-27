e985590acafe8dfd3acf4c3cb6fdae420640c1a0
Jon Nordling <jonnordling@gmail.com>
2017-07-27 12:21:31 -0400

    Adding dev/production parms for development

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloMap from './components/HelloMap';
-require("../css/app.styl")
+import {config as config} from './config';
+console.log(config.production)
+//Only for development
+if (!config.production){
+  require("../css/app.styl")
+}
 
 ReactDOM.render(
   <HelloMap />,
